63261193bebedfdf8e78d9619220c07f0913c530
Vladislav Kochkin <ltrickfaim@gmail.com>
2020-10-28 16:31:32 +0600

    Настроил редакс

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {applyMiddleware, compose, createStore} from "redux";
+import thunkMiddleware from 'redux-thunk';
+import {Provider} from "react-redux";
+import {linkReducer} from "./store/linkReducer";
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+    linkReducer,
+    composeEnhancers(applyMiddleware(thunkMiddleware))
+);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
