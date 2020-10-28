8439b5bc77fa05569f37ace50e4b6449df98e8cd
Vladislav Kochkin <ltrickfaim@gmail.com>
2020-10-28 16:44:06 +0600

    Добавил обработку ошибок

--- frontend/src/store/linkActions.js
+++ frontend/src/store/linkActions.js
@@ -1,25 +1,26 @@
 import {
     CHANGE_VALUE,
     POST_ORIGINAL_LINK_ERROR,
-    POST_ORIGINAL_LINK_REQUEST,
     POST_ORIGINAL_LINK_SUCCESS
 } from "./actionTypes";
 import {axiosApi} from "../axiosApi";
 
 export const changeValue = e => ({type: CHANGE_VALUE, value: e.target.value});
-const postOriginalLinkRequest = () => ({type: POST_ORIGINAL_LINK_REQUEST});
 const postOriginalLinkSuccess = data => ({type: POST_ORIGINAL_LINK_SUCCESS, data});
 const postOriginalLinkError = error => ({type: POST_ORIGINAL_LINK_ERROR, error});
 
 export const postOriginalLink = (e, data) => {
     e.preventDefault();
     return async dispatch => {
-        dispatch(postOriginalLinkRequest());
         try {
             const response = await axiosApi.post(`/links`, data);
             dispatch(postOriginalLinkSuccess(response.data));
         } catch (e) {
-            dispatch(postOriginalLinkError(e));
+            if(e.message === 'Network error') {
+                dispatch(postOriginalLinkError(e.message));
+            } else {
+                dispatch(postOriginalLinkError(e.response.data._message));
+            }
         }
     }
 }
--- frontend/src/store/linkReducer.js
+++ frontend/src/store/linkReducer.js
@@ -1,36 +1,27 @@
 import {
     CHANGE_VALUE,
     POST_ORIGINAL_LINK_ERROR,
-    POST_ORIGINAL_LINK_REQUEST,
     POST_ORIGINAL_LINK_SUCCESS
 } from "./actionTypes";
 
 const initialState = {
     link: null,
     originalUrl: '',
-    loading: false,
     error: null
 };
 
 export const linkReducer = (state = initialState, action) => {
     switch (action.type) {
-        case POST_ORIGINAL_LINK_REQUEST:
-            return {
-                ...state,
-                loading: true
-            };
         case POST_ORIGINAL_LINK_SUCCESS:
             return {
                 ...state,
                 link: action.data,
-                loading: false,
                 error: null,
                 originalUrl: ''
             };
         case POST_ORIGINAL_LINK_ERROR:
             return {
                 ...state,
-                loading: false,
                 error: action.error
             };
         case CHANGE_VALUE:
