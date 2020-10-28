f81eb83e4c0ff59ca26195ddad7de2268a363182
Vladislav Kochkin <ltrickfaim@gmail.com>
2020-10-28 16:44:23 +0600

    Вывод страницы

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import Main from "./containers/Main/Main";
 
 function App() {
   return (
-    <div className="App">
-      <h1>asdas</h1>
-    </div>
+    <>
+      <Main />
+    </>
   );
 }
 
